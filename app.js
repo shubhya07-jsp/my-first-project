cc82a93a2867101c2b82f44185715ff8830bf713
Shubham Patil <bhaupatil0734@gmail.com>
2026-04-10 14:16:21 +0530

    Add HTTP server to app.js
    my file

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 my name is shubham patil
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.write("Hello from AWS 🚀");
+  res.end();
+}).listen(3000);
+
+console.log("Server running on port 3000");
